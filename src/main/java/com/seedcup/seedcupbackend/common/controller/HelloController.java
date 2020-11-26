838b3d4b229f79516a1899b59eb98aa069ced969
holdice <hemu0710@qq.com>
2020-11-26 16:42:09 +0800

    remove swagger2 api doc use yapi doc(url will be in README.md)
    Signed-off-by: holdice <hemu0710@qq.com>

--- src/main/java/com/seedcup/seedcupbackend/common/controller/HelloController.java
+++ src/main/java/com/seedcup/seedcupbackend/common/controller/HelloController.java
@@ -2,29 +2,21 @@ package com.seedcup.seedcupbackend.common.controller;
 
 import com.seedcup.seedcupbackend.common.annotation.LoginRequired;
 import com.seedcup.seedcupbackend.common.interceptor.AuthInterceptor;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
-@Api(tags = "测试用示范接口")
 @RequestMapping(value = "/api/test")
 public class HelloController {
 
     @LoginRequired
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @ApiOperation(value = "测试接口，hello，world")
     public String index(HttpSession session) {
         /*
          * @Author holdice
--- src/main/java/com/seedcup/seedcupbackend/common/controller/UserController.java
+++ src/main/java/com/seedcup/seedcupbackend/common/controller/UserController.java
@@ -9,7 +9,6 @@ import com.seedcup.seedcupbackend.common.po.User;
 import com.seedcup.seedcupbackend.common.service.UserService;
 import com.seedcup.seedcupbackend.global.dto.ResponseDto;
 import com.seedcup.seedcupbackend.global.dto.StandardResponse;
-import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,19 +18,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@Api(tags = "用户相关路由")
 @RequestMapping(value = "/api/user", produces = "application/json")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
-    @ApiOperation(value = "用户注册", notes = "注意用户注册参数合法性,如果遇到注册用户名、邮箱或者电话号码已存在，则返回code=101，并在data里附上重复的字段名list")
-    @ApiResponses({
-            @ApiResponse(code = 101, message = "duplicate user information",
-                    response = String.class, responseContainer = "list")
-    })
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public ResponseDto<Object> signUp(@Valid @RequestBody UserSignUpDto signUpDto) {
         /*
          * @Author holdice
@@ -48,8 +41,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value = "/logIn", method = RequestMethod.POST)
-    @ApiOperation(value = "用户登录", notes = "使用session维持用户登录状态", code = 0)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseDto<Object> logIn(@Valid @RequestBody UserLoginDto loginInfo, HttpSession session) {
         User user  = userService.logIn(loginInfo);
         if (user != null) {
@@ -61,8 +53,7 @@ public class UserController {
     }
 
     @LoginRequired
-    @RequestMapping(value = "/logOut", method = RequestMethod.GET)
-    @ApiOperation(value = "注销登录", notes = "需要登录，即没登录的时候无权调用此接口")
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ResponseDto<Object> logOut(HttpSession session) {
         userService.logOut(session);
         return StandardResponse.ok();
