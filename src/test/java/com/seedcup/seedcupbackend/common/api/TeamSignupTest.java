f4019ca86374205029ee876325e0de3e845726e5
Husterjin <1444865009@qq.com>
2020-12-10 16:14:40 +0800

    feat: 获取队伍中所有成员列表

---
+++ src/test/java/com/seedcup/seedcupbackend/common/api/TeamSignupTest.java
@@ -0,0 +1,53 @@
+package com.seedcup.seedcupbackend.common.api;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.seedcup.seedcupbackend.ApiUtils;
+import com.seedcup.seedcupbackend.common.dao.TeamMapper;
+import com.seedcup.seedcupbackend.common.po.Team;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TeamSignupTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TeamMapper teamMapper;
+
+    @Test
+    public void signUp1() throws Exception{
+        var request = ApiUtils.postBuilder("/api/user/log_in")
+                .content("{\n" +
+                        "  \"username\": \"admin01@admin.com\",\n" +
+                        "  \"password\": \"admin01\"\n" +
+                        "}");
+        var result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("0"))
+                .andReturn();
+        request = ApiUtils.postBuilder("/api/team/signup")
+                .content("{\n" +
+                        "  \"teamName\": \"test111\",\n" +
+                        "  \"highestGrade\": \"2018\",\n" +
+                        "  \"introduction\": \"111\"\n" +
+                        "}")
+                .cookie(result.getResponse().getCookies()[0]);
+        mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("0"));
+    }
+
+    @AfterEach
+    public void closeDB(){
+        QueryWrapper<Team> qw = new QueryWrapper<>();
+        qw.like("name", "test");
+        System.out.println("after test, " + teamMapper.delete(qw) + "record(s) has been deleted");
+    }
+}
+
