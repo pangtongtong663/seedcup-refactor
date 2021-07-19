2484293e8e0ce58676dc871918c0a06cc1fa860f
holdice <hemu0710@qq.com>
2021-07-19 19:56:40 +0800

    refactor(auth): del session

--- src/main/java/com/seedcup/backend/global/config/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package com.seedcup.backend.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
-public class SessionConfig {
-}
